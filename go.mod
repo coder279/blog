ddfb628a0d8281f0ba5acfa13efd85dd5a082d72
TimePHPer <244605960@qq.com>
2020-12-29 22:04:32 +0800

    数据库设计,数据模型设计,路由初始化配置,验证

--- go.mod
+++ go.mod
@@ -4,5 +4,5 @@ go 1.14
 
 require (
 	github.com/gin-gonic/gin v1.6.3
-	github.com/go-playground/universal-translator v0.17.0
+	github.com/go-playground/universal-translator v0.17.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,17 +1,19 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	"myblog/internal/routers"
 	"net/http"
+	"time"
 )
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping",func(c *gin.Context){
-		c.JSON(200,gin.H{
-			"message":"message",
-		})
-	})
-	go http.ListenAndServe("127.0.0.1:8081",http.DefaultServeMux)
-	r.Run()
+	router := routers.NewRouter()
+	s := &http.Server{
+		Addr: ":8080",
+		Handler: router,
+		ReadTimeout: 10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		MaxHeaderBytes: 1<<20,
+	}
+	s.ListenAndServe()
 }
