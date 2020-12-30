8cffdd0a2896d3a3b9a4765a0f833f96f060db20
TimePHPer <244605960@qq.com>
2020-12-30 23:28:38 +0800

    配置管理

---
+++ global/setting.go
@@ -0,0 +1,10 @@
+package global
+
+import "myblog/pkg/setting"
+
+var (
+	ServerSetting *setting.AppSettings
+	AppSetting *setting.ServerSettings
+	DataBaseSetting *setting.DatabaseSettings
+)
+
---
+++ pkg/setting/section.go
@@ -0,0 +1,38 @@
+package setting
+
+import "time"
+
+type ServerSettings struct {
+	RunMode string
+	HttpPort string
+	ReadTimeOut time.Duration
+	WriteTimeOut time.Duration
+}
+type AppSettings struct {
+	DefaultPageSize int
+	MaxPageSize int
+	LogSavePath string
+	LogFileName string
+	LogFileExt string
+}
+type DatabaseSettings struct {
+	DBType       string
+	UserName     string
+	Password     string
+	Host         string
+	DBName       string
+	TablePrefix  string
+	Charset      string
+	ParseTime    bool
+	MaxIdleConns int
+	MaxOpenConns int
+}
+
+func (s *Setting) ReadSection(k string, v interface{}) error{
+	err := s.vp.UnmarshalKey(k,v)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
