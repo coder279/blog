3847ed6200234c221870b859d1a18d771e10e047
TimePHPer <244605960@qq.com>
2020-12-30 22:54:28 +0800

    错误码标准化

--- pkg/errcode/errcode.go
+++ pkg/errcode/errcode.go
@@ -1,6 +1,9 @@
 package errcode
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 type Error struct {
 	code int `json:"code"`
@@ -16,3 +19,54 @@ func NewError(code int,msg string) *Error{
 	codes[code] = msg
 	return &Error{code:code,msg:msg}
 }
+
+func (e *Error) Error() string{
+	return fmt.Sprintf("错误码: %d,错误信息: %s",e.Code(),e.Msg())
+}
+func (e *Error) Code() int {
+	return e.code
+}
+
+func (e *Error) Msg() string {
+	return e.msg
+}
+
+func (e *Error) Msgf(args []interface{}) string {
+	return fmt.Sprintf(e.msg, args...)
+}
+
+func (e *Error) Details() []string {
+	return e.details
+}
+
+func (e *Error) WithDetails(details ...string) *Error{
+	e.details = []string{}
+	for _,d := range details{
+		e.details = append(e.details,d)
+	}
+	return e
+}
+
+func (e *Error) StatusCode() int {
+	switch e.Code() {
+	case Success.Code():
+		return http.StatusOK
+	case ServerError.Code():
+		return http.StatusInternalServerError
+	case InvalidParams.Code():
+		return http.StatusBadRequest
+	case UnauthorizedAuthNotExist.Code():
+		fallthrough
+	case UnauthorizedTokenError.Code():
+		fallthrough
+	case UnauthorizedTokenGenerate.Code():
+		fallthrough
+	case UnauthorizedTokenTimeout.Code():
+		return http.StatusUnauthorized
+	case TooManyRequests.Code():
+		return http.StatusTooManyRequests
+	}
+	return http.StatusInternalServerError
+
+}
+
